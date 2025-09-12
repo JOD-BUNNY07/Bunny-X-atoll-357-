b5cadf9ddb7fb8717486d7d14564c65c98677565
theshaenix <theshaenix@gmail.com>
2025-09-12 21:03:59 +0530

    sched/fair: bore: sched_burst_smoothness_short = 1
    (cherry picked from commit c25c3221766cfdd160051ad5196bdb3f1c09e85a)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -221,7 +221,7 @@ uint __read_mostly sched_burst_cache_lifetime   = 60000000;
 
 #ifdef CONFIG_SCHED_BORE
 uint __read_mostly sched_burst_smoothness_long  = 1;
-uint __read_mostly sched_burst_smoothness_short = 0;
+uint __read_mostly sched_burst_smoothness_short = 1;
 uint __read_mostly sched_burst_fork_atavistic   = 2;
 uint __read_mostly sched_burst_penalty_offset   = 22;
 uint __read_mostly sched_burst_penalty_scale    = 1280;
