e537b78a267398380f8a94eee150a877216f0769
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-12-14 10:14:04 +0800

    fs: implement susfs v2.0.0
    * sus_mount and others are complied with the upstream susfs kernel branch v5.10
    * susfs try_umount is restored
    * auto features are deprecated so you have to hide your mounts manually
    
    This susfs backport commit requires a set of backports to comply with the upstream susfs:
    d406904bb175cb9791b5aed4404df3268859d91b fs: Convert namespace IDAs to new API
    9ba899398576d329288b023566d3a731470ad13a fs: Convert unnamed_dev_ida to new API
    e784224542fab4b3888d9103a1f678015221a7d1 ida: Add new API
    ed8fd10d97a50ec424ae38a40351b5de060f9838 ida: Lock the IDA in ida_destroy
    4ba6dccf73a620e565803e5995d5748ecfbb56a2 lib/idr.c: remove simple_ida_loc
    0a8cbf3 proc: less memory for /proc/*/map_files readdir
    37ae244 mm: smaps: split PSS into components
    49a5115 fs/task_mmu: add pkeys header
    6f94042 mm/pkeys: Add an empty arch_pkeys_enabled()
    bbd5aec mm/pkeys, powerpc, x86: Provide an empty vma_pkey() in linux/pkeys.h
    849ca8c mm: /proc/pid/smaps_rollup: convert to single value seq_file
    6071a48 mm: /proc/pid/smaps: factor out common stats printing
    03fd2fb mm: /proc/pid/smaps: factor out mem stats gathering
    95f8be4 mm: /proc/pid/*maps remove is_pid and related wrappers
    27956d2 procfs: add seq_put_hex_ll to speed up /proc/pid/maps
    7f2847d proc: add seq_put_decimal_ull_width to speed up /proc/pid/smaps
    
    For KernelSU, Use my RKSU fork which it includes susfs try_umount:
    https://github.com/sidex15/KernelSU/tree/susfs-rksu-master
    
    Or if you want to patch it within RKSU, use this commit:
    https://github.com/sidex15/KernelSU/commit/8be13a387a7b32f6c70b9077a97c30840254f630
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1190,7 +1190,7 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
-static int override_version(struct new_utsname __user *name)
+atic int override_version(struct new_utsname __user *name)
 {
 #ifdef CONFIG_F2FS_REPORT_FAKE_KERNEL_VERSION
 	int ret;
@@ -1212,13 +1212,19 @@ static int override_version(struct new_utsname __user *name)
 #endif
 }
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern void susfs_spoof_uname(struct new_utsname* tmp);
+#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (!strncmp(current->comm, "bpfloader", 9) ||
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	susfs_spoof_uname(&tmp);
+#endif
+f (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "uprobestatsbpfload", TASK_COMM_LEN - 1) ||
 	    !strncmp(current->comm, "netd", 4)) {
