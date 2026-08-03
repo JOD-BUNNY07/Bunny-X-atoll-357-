a6f8753a0178416a0a3ed05d9d0e406584916fcc
JOD-BUNNY07 <dassouradeep3310@gmail.com>
2026-08-03 07:20:49 +0530

    Refactor KSU hook handling in read syscall

--- fs/read_write.c
+++ fs/read_write.c
@@ -584,9 +584,8 @@ static inline void file_pos_write(struct file *file, loff_t pos)
 }
 
 #ifdef CONFIG_KSU
-extern bool ksu_init_rc_hook __read_mostly;
-extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
-				char __user **buf_ptr, size_t *count_ptr);
+extern struct static_key_true ksu_is_init_rc_hook_enabled;
+extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd);
 #endif
 
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
@@ -598,8 +597,8 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 #endif /*OPLUS_FEATURE_IOMONITOR*/
 
 #ifdef CONFIG_KSU
-	if (unlikely(ksu_init_rc_hook))
-		ksu_handle_sys_read(fd, &buf, &count);
+	if (static_branch_unlikely(&ksu_is_init_rc_hook_enabled))
+    ksu_handle_sys_read(fd);
 #endif
 	if (f.file) {
 		loff_t pos = file_pos_read(f.file);
