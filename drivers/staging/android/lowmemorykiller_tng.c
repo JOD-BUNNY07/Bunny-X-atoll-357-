241bbdbd888b60efcbc1998f256923bbf7612bd0
JOD-BUNNY07 <dassouradeep3310@gmail.com>
2026-08-04 09:29:49 +0530

    android: fix free_area access for LMK TNG

--- drivers/staging/android/lowmemorykiller_tng.c
+++ drivers/staging/android/lowmemorykiller_tng.c
@@ -83,7 +83,7 @@ static inline long zone_threshold_check(struct zone *zone, int zone_type,
 		struct free_area *area;
 		struct list_head *curr;
 
-		area = &zone->free_area[order];
+		area = &zone->free_area[zone_type][order];
 		list_for_each(curr, &area->free_list[zone_type]) {
 			tot += 1 << order;
 			if (tot >= threshold)
