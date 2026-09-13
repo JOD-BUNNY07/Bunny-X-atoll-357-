f15d958799168189e6c81f1d961228d65f019ef7
simonpunk <simonpunk2016@gmail.com>
2026-09-13 16:57:03 +0800

    fs: susfs: Refactor spoofing logic of SUS_MOUNT, SUS_KSTAT and OPEN_REDIRECT
    /* Objective */
    - This commit contains two parts:
    1. SUS_MOUNT no longer relies on kernel_umount. we no longer need to add sus mounts to kernel_umount, which means we can disable kernel_umount in KSU manager and mounts are still hidden, but it is still safe to have them umounted. Besides, we can save a lot of overheads where we have to umount those sus mounts after zygote unshares.
    
    2. Make SUS_MOUNT, SUS_KSTAT and OPEN_REDIRECT work independently without interfering with each other, while they can also work all together to achieve other types of hiding like when the mounted/redirected path needs to be visible to app process.
    
    /* kernel changes */
    [SUS_MOUNT]
    - Now SUS_MOUNT will be always effective for process that is marked with TIF_PROC_UMOUNTED.
    
    - Spoofing __lookup_mnt() for zygote spawned app process as well, so process that is marked with TIF_PROC_MOUNTED will only reach the non-sus-mounted path without having them umounted, no matter it is spwaned by zygote or zygote_next or not.
    
    - Change all "static_branch_unlikely" instances in proc_namespace.c to "static_branch_likely" since we need to have it enabled most of the time anyway.
    
    [SUS_KSTAT]
    - Now SUS_KSTAT will be always effective for process with (uid % 100000) >= 10000. Since sometimes we need the sus mounts or redirected path to stay for target app process, we can only rely on SUS_KSTAT to hide the stat for the mounted/redirected path.
    
    - Log the target_dev and spoofed_mnt_id for new entry as well.
    
    - Expand SUS_KSTAT_HLIST bucket size to 14bits, to make sure we get the fatest lookup as much as possible as we use only hash_for_each_possible() anyway, and a 128 KB memory cost is not a concern.
    
    - Place hlist_node and the lookup key "target_ino" at the top of the struct, to make sure it pulls 1 cache line into L1 cache.
    
    [OPEN_REDIRECT]
    - For added redirected paths which are also visible to (uid % 100000) >= 10000, SUS_KSTAT is required to hide from other detections since OPEN_REDIRECT itself cannot fully spoof the kstat, kstatfs, inotify fdinfo and proc fd.
    
    - Expand OPEN_REDIRECT_HLIST bucket size to 14bits.
    
    - Place hlist_node and the lookup key "target_ino" at the top of the struct.
    
    Note: this is just a semantic port as most of the functions and symbols (e.g STATX_MNT_ID, zygote_next, etc) did not exist in 4.14.
    
    co-authored-by: sidex15 <24408329+sidex15@users.noreply.github.com>

--- include/linux/susfs_def.h
+++ include/linux/susfs_def.h
@@ -76,13 +76,21 @@
 #define ND_STATE_OPEN_LAST 64
 #define ND_STATE_LAST_SDCARD_SUS_PATH 128
 #define ND_FLAGS_LOOKUP_LAST		0x2000000
- 
+
+/* - statx request_mask => storing flag 'STATX_' (used internally by susfs, cleared before reaching userspace) */
+#define STATX_SUS_KSTAT 0x10000000U
+#define STATX_SUS_KSTAT_FUSE 0x20000000U
+
 #define MAGIC_MOUNT_WORKDIR "/debug_ramdisk/workdir"
 #define DEFAULT_KSU_MNT_MINOR_DEV (1 << 12) /* should be way enough, here minor(dev) begins with 4097 */
 
 #ifndef FUSE_SUPER_MAGIC
 #define FUSE_SUPER_MAGIC 0x65735546
 #endif
+
+static inline bool susfs_is_current_app_uid(void) {
+	return ((current_uid().val % 100000) >= 10000);
+}
 
 static inline bool susfs_is_current_proc_umounted(void) {
 	return test_ti_thread_flag(&current->thread_info, TIF_PROC_UMOUNTED);
