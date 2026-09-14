c83209459ba9e419a45dbc25905db638b0e6be28
JOD-BUNNY07 <dassouradeep3310@gmail.com>
2026-09-14 17:23:38 +0530

    SUSFS: fix hide sus mounts static key

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -147,12 +147,12 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (READ_ONCE(susfs_hide_sus_mnts_for_non_su_procs) &&
-			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
-			!susfs_is_current_ksu_domain())
-	{
-		return 0;
-	}
+if (static_branch_unlikely(
+                        &susfs_is_hide_sus_mnts_for_non_su_procs_enabled) &&
+                        r->mnt_id >= DEFAULT_KSU_MNT_ID &&
+                        !susfs_is_current_ksu_domain())
+        {
+}
 #endif
 
 	seq_printf(m, "%i %i %u:%u ", r->mnt_id, r->mnt_parent->mnt_id,
